bc0f20fecd8d0c5c8c1b997590487de3131cc97a
Nolan Jiang <scargtt@163.com>
2017-05-04 23:54:46 +0800

    generate / solve

--- prim.js
+++ prim.js
@@ -1,8 +1,18 @@
-function Maze(col, row, start, end) {
+/**
+ *
+ * @param row 行
+ * @param col 列
+ * @param start[x, y] 起点坐标
+ * @param end[终点坐标] 终点坐标
+ * @param speed 动画速度
+ * @constructor 迷宫类
+ */
+function Maze(row, col,  start, end, speed = 10) {
     this.col = col;
     this.row = row;
     this.start = start;
     this.end = end;
+    this.speed = speed;
 }
 
 Maze.prototype.random = function (k) {
@@ -19,15 +29,15 @@ Maze.prototype.generate = function () {
             if (i % 2 == 0 || j % 2 == 0) {
                 arr.push({
                     value: 0,
-                    x: j,
-                    y: i
+                    i: i,
+                    j: j
                 });
             } else {
                 arr.push({
                     value: 1,
                     isVisited: false,
-                    x: j,
-                    y: i
+                    i: i,
+                    j: j
                 });
             }
         }
@@ -42,10 +52,10 @@ Maze.prototype.generate = function () {
     // 循环以下操作，直到所有的格子都被访问到。
     while (currentNode.isVisited) {
         // 得到当前访问格子的四周（上下左右）的格子
-        let upNode = this.mazeDataArray[currentNode.y - 2] ? this.mazeDataArray[currentNode.y - 2][currentNode.x] : {isVisited: true};
-        let rightNode = this.mazeDataArray[currentNode.x + 2] ? this.mazeDataArray[currentNode.y][currentNode.x + 2] : {isVisited: true};
-        let downNode = this.mazeDataArray[currentNode.y + 2] ? this.mazeDataArray[currentNode.y + 2][currentNode.x] : {isVisited: true};
-        let leftNode = this.mazeDataArray[currentNode.x - 2] ? this.mazeDataArray[currentNode.y][currentNode.x - 2] : {isVisited: true};
+        let upNode = this.mazeDataArray[currentNode.i - 2] ? this.mazeDataArray[currentNode.i - 2][currentNode.j] : {isVisited: true};
+        let rightNode = this.mazeDataArray[currentNode.j + 2] ? this.mazeDataArray[currentNode.i][currentNode.j + 2] : {isVisited: true};
+        let downNode = this.mazeDataArray[currentNode.i + 2] ? this.mazeDataArray[currentNode.i + 2][currentNode.j] : {isVisited: true};
+        let leftNode = this.mazeDataArray[currentNode.j - 2] ? this.mazeDataArray[currentNode.i][currentNode.j - 2] : {isVisited: true};
 
         let neighborArray = [];
         if (!upNode.isVisited) {
@@ -64,7 +74,7 @@ Maze.prototype.generate = function () {
         // 如果找到，则把该格子和当前访问的格子中间的墙打通(置为0)，
         if (neighborArray.length !== 0) { // 如果找到
             let neighborNode = neighborArray[this.random(neighborArray.length)];
-            this.mazeDataArray[(neighborNode.y + currentNode.y) / 2][(neighborNode.x + currentNode.x) / 2].value = 1;
+            this.mazeDataArray[(neighborNode.j + currentNode.j) / 2][(neighborNode.i + currentNode.i) / 2].value = 1;
             neighborNode.isVisited = true;
             visitedList.push(neighborNode);
             currentNode = neighborNode;
@@ -89,18 +99,19 @@ Maze.prototype.generate = function () {
     }
     //start 0,0
     // 1,0 => 0
-    this.mazeDataArray[this.start[0]][this.start[1]]={
-        x:this.start[0],
-        y:this.start[1],
-        value:1
+    this.mazeDataArray[this.start[0]][this.start[1]] = {
+        i: this.start[0],
+        j: this.start[1],
+        value: 1
     };
     // this.mazeDataArray[this.start[0]+1][this.start[0]+1]={value:1};
     // end 9,9
     // 20,21 =>1
-    this.mazeDataArray[this.end[0]][this.end[1]]={
-        x:this.end[0],
-        y:this.end[1],
-        value:1};
+    this.mazeDataArray[this.end[0]][this.end[1]] = {
+        i: this.end[0],
+        j: this.end[1],
+        value: 1
+    };
     // this.mazeDataArray[this.end[0]][this.end[1]-1]={value:1};
 };
 
@@ -112,17 +123,17 @@ Maze.prototype.drawDom = function () {
         for (let j = 0, len = this.mazeDataArray[i].length; j < len; j++) {
             let td = document.createElement("td");
             // start
-            if(i===this.start[0] && j===this.start[1] ){
+            if (i === this.start[0] && j === this.start[1]) {
                 td.setAttribute("class", "startNode");
-                td.innerHTML= 's';
+                td.innerHTML = 's';
             }
             // end
-            if(i===this.end[0] && j===this.end[1] ){
+            if (i === this.end[0] && j === this.end[1]) {
                 td.setAttribute("class", "endNode");
-                td.innerHTML= 'e';
+                td.innerHTML = 'e';
             }
             // wall
-            if (!this.mazeDataArray[i][j].value){
+            if (!this.mazeDataArray[i][j].value) {
                 td.setAttribute("class", "wall");
             }
 
@@ -140,70 +151,87 @@ Maze.prototype.findPath = function () {
     }
     // 路径数组
     this.path = [];
+    let node = this.mazeDataArray[this.start[0]][this.start[1]]; // 迷宫的出口
+    // let pathTree = node;
+    let queue = []; // 辅助队列
+    node.isVisited = true;
+    queue.unshift(node); // 入队
+    while (queue.length) { // 队列非空
+        let firstItem = queue.shift(); // 队首元素 出队
+        firstItem.neighbor = [];
+        if (this.mazeDataArray[node.i - 1] && this.mazeDataArray[node.i - 1][node.j].value) {// 上
+            if (!this.mazeDataArray[node.i - 1][node.j].isVisited) {
+                firstItem.neighbor.push(this.mazeDataArray[node.i - 1][node.j]);
+                // 记录前置节点的坐标，以便最后打印出路径
+                this.mazeDataArray[node.i - 1][node.j].pre = [firstItem.i, firstItem.j];
+            }
 
-    let node = this.mazeDataArray[this.end[0]][this.end[1]]; // 迷宫的出口
+        }
+        if (this.mazeDataArray[node.i][node.j + 1] && this.mazeDataArray[node.i][node.j + 1].value) {// 右
+            if (!this.mazeDataArray[node.i][node.j + 1].isVisited) {
+                firstItem.neighbor.push(this.mazeDataArray[node.i][node.j + 1]);
+                this.mazeDataArray[node.i][node.j + 1].pre = [firstItem.i, firstItem.j];
+            }
 
-    while (node) {
-        // console(a)
-        let queue = []; // 辅助队列
+        }
+        if (this.mazeDataArray[node.i + 1] && this.mazeDataArray[node.i + 1][node.j].value) {// 下
+            if (!this.mazeDataArray[node.i + 1][node.j].isVisited) {
+                firstItem.neighbor.push(this.mazeDataArray[node.i + 1][node.j]);
+                this.mazeDataArray[node.i + 1][node.j].pre = [firstItem.i, firstItem.j];
+            }
 
-        if (!node.isVisited) {
-            node.isVisited = true;
-            // console.log(node)
-            queue.unshift(node); // 入队
-            while (queue.length) { // 队列非空
-                // console.log(queue.length);
-                let firstItem = queue.shift(); // 队首元素 出队
-                // console.log(firstItem);
-                firstItem.neighbor = [];
-                if (this.mazeDataArray[node.x - 1] && this.mazeDataArray[node.x - 1][node.y].value) {// 上
-                    if (!this.mazeDataArray[node.x - 1][node.y].isVisited)
-                        firstItem.neighbor.push(this.mazeDataArray[node.x - 1][node.y]);
-                }
-                if (this.mazeDataArray[node.x][node.y + 1] && this.mazeDataArray[node.x][node.y + 1].value) {// 右
-                    if (!this.mazeDataArray[node.x][node.y + 1].isVisited)
-                        firstItem.neighbor.push(this.mazeDataArray[node.x][node.y + 1]);
-                }
-                if (this.mazeDataArray[node.x + 1] && this.mazeDataArray[node.x + 1][node.y].value) {// 下
-                    if (!this.mazeDataArray[node.x + 1][node.y].isVisited)
-                        firstItem.neighbor.push(this.mazeDataArray[node.x + 1][node.y]);
-                }
-                if (this.mazeDataArray[node.x][node.y - 1] && this.mazeDataArray[node.x][node.y - 1].value) {// 左
-                    if (!this.mazeDataArray[node.x][node.y - 1].isVisited)
-                        firstItem.neighbor.push(this.mazeDataArray[node.x][node.y - 1]);
-                }
+        }
+        if (this.mazeDataArray[node.i][node.j - 1] && this.mazeDataArray[node.i][node.j - 1].value) {// 左
+            if (!this.mazeDataArray[node.i][node.j - 1].isVisited) {
+                firstItem.neighbor.push(this.mazeDataArray[node.i][node.j - 1]);
+                this.mazeDataArray[node.i][node.j - 1].pre = [firstItem.i, firstItem.j];
+            }
+        }
+        // 遍历邻居节点
+        for (neighborNode of firstItem.neighbor) {
+            if (!neighborNode.isVisited) {
+                neighborNode.isVisited = true;
+                queue.push(neighborNode);
+            }
 
-                // console.log(u.neighbor);
-                // 遍历邻居节点
-                for (neighborNode of firstItem.neighbor) {
-                    if (!neighborNode.isVisited) {
-                        neighborNode.isVisited = true;
-                        queue.push(neighborNode);
-                    }
-                }// for 遍历邻居节点
+        }
+        node = queue[0];
+        if (node && node.i === this.end[0] && node.j === this.end[1]) {
+            this.pre = node;
+            break;
+        }
+    }// while
 
-                node = queue[0];
-                this.path.push(node);
-                // console.log(node);
-            }// while
-        }// visited
+    let item = this.pre;
+
+    while (item.pre) {
+        this.path.unshift([item.pre[0], item.pre[1]]);
+        // 生成路径数组，path[0]为起点坐标
+        item = this.mazeDataArray[item.pre[0]][item.pre[1]];
     }
+};
 
-    let pathTree = this.path[0];
+Maze.prototype.Animation = function () {
+    let count = 0;
 
-    console.log(pathTree);
-
-    /*let pathArray = [];
-     while (endNode) {
-     pathArray.push([endNode.x, endNode.y])
-     endNode = endNode.pre;
-     }
-     console.log(pathArray)*/
-
+    function animation(pathArray) {
+        if (count < pathArray.length) {
+            let trArr = document.getElementsByTagName('tr');
+            let tdArr = trArr[pathArray[count][0]].getElementsByTagName('td');
+            let cell = tdArr[pathArray[count][1]];
+            cell.setAttribute("class", "path");
+            count++
+        } else {
+            clearInterval(sh);
+            console.log('finded the path')
+        }
+    }
+    let tem = setInterval(animation, this.speed, this.path);
 };
 
 Maze.prototype.init = function () {
     this.generate();
     this.drawDom();
     this.findPath();
+    // this.Animation();
 };
